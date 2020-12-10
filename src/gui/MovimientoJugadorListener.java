e9060eebe655ddb61c57764a3025444df3821866
nic <nicolasborromei@gmail.com>
2020-12-10 15:03:02 -0300

    documentacion listener

--- src/gui/MovimientoJugadorListener.java
+++ src/gui/MovimientoJugadorListener.java
@@ -6,13 +6,28 @@ import java.awt.event.KeyListener;
 import entidades.EntidadGrafica;
 import entidades.personajes.Jugador;
 
+/**
+ * Listener movimiento del jugador
+ * @author nicob
+ *
+ */
 public class MovimientoJugadorListener implements KeyListener {
+	/**
+	 * El jugador
+	 */
 	Jugador jugador;
 
+	/**
+	 * Crea un nuevo movimientoJugadorListener
+	 * @param jugador El jugador al que se atribuye el movimiento
+	 */
 	public MovimientoJugadorListener(Jugador jugador) {
 		this.jugador = jugador;
 	}
-
+	
+	/**
+	 * Dada una tecla presionada, reacciona generando movimiento y el disparo
+	 */
 	public void keyPressed(KeyEvent e) {
 		if (e.getKeyCode() == KeyEvent.VK_RIGHT
 				&& !fueraDelMapa(jugador.getEntidadGrafica().getX() + jugador.getVelocidad())) {
@@ -34,7 +49,12 @@ public class MovimientoJugadorListener implements KeyListener {
 	public void keyReleased(KeyEvent e) {
 		jugador.mover(0);
 	}
-
+	
+	/**
+	 * Dado un valor x evuelve true si el jugador se encuentra fuera del mapa
+	 * @param x El valor x
+	 * @return true si se encuentra fuera del mapa
+	 */
 	public boolean fueraDelMapa(int x) {
 		boolean toret = false;
 		if (x < 0 || x + jugador.getEntidadGrafica().getLabel().getWidth() > jugador.getJuego().getMapa().getWidth())
